4d1ad2b3203ab59aade201ac22bd516ef6f47ead
hamin <kyden5386@gmail.com>
2026-04-04 16:00:31 +0900

    Problem: 11399

--- src/greedy/ATM.java
+++ src/greedy/ATM.java
@@ -1,10 +1,13 @@
+package greedy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class ATM {
+    // ATM, https://www.acmicpc.net/problem/11399
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
