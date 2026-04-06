50637133b0991423588b4b7c434b8267022229f3
hamin <kyden5386@gmail.com>
2026-04-06 20:21:10 +0900

    0406

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] p = new int[N];
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] A = new int[N+1];
+        int numOfCoin = 0;
 
-        for (int i = 0; i < N; i++) {
-            p[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(p);
-        int sum=0, res=0;
-        for (int i = 0; i < N; i++) {
-            sum += p[i];
-            res += sum;
-        }
-        System.out.println(res);
     }
 }
