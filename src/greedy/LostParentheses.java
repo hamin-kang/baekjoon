67b6f1b3325963f76c36e940924076608f1afe54
hamin <kyden5386@gmail.com>
2026-04-13 22:41:33 +0900

    Problem: 1541

--- src/greedy/LostParentheses.java
+++ src/greedy/LostParentheses.java
@@ -1,8 +1,11 @@
+package greedy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class LostParentheses {
+    // 잃어버린 괄호, https://www.acmicpc.net/problem/1541
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
