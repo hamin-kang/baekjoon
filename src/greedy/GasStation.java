b1f6c4c73d3d7374679e74615458755999be5032
hamin <kyden5386@gmail.com>
2026-04-15 23:34:33 +0900

    Problem: 13305

--- src/greedy/GasStation.java
+++ src/greedy/GasStation.java
@@ -1,9 +1,12 @@
+package greedy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class GasStation {
+    // 주유소, https://www.acmicpc.net/problem/13305
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine()); // 도시의 개수
